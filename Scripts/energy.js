70ad7066107ca897515f6c32eafcfc2f89538138
andrewj35 <44450807+andrewj35@users.noreply.github.com>
2018-11-20 19:16:04 -0800

    Update energy.js
    fixed the power bar so it doesn't allow us to buy it if we don't have enough money or consumes it

--- Scripts/energy.js
+++ Scripts/energy.js
@@ -51,8 +51,15 @@ function energyCost(x,y){
 		case "tree" : cost += checkToolBag("tree");
 					removeItemFromMap(x,y);
 					break;
-//		case "power-bar" :checkForPurchase(x,y);
-//					break;			
+		case "power-bar" : if(money == 0) {
+					alert("Can't buy the power bar, you are too damn poor. Get some more whiffles and come back!");
+					break;
+				   }
+				   	cost -= 20;
+					money -= 1;
+					whiffles.innerHTML = "Whiffles: " +money;
+					removeItemFromMap(x,y);
+					break;			
 		default: break;
 	}
 	}
