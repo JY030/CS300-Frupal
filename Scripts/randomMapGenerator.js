10836ee6667d5000120f6b06eeafe65cfb017250
jneckar <44450838+jneckar@users.noreply.github.com>
2018-11-27 15:38:26 -0800

    Update randomMapGenerator.js
    added comments for clarity, made a few adjustments to spawn rates. spawn rate of objects can now be adjusted based on the terrain type. forest tiles are now more likely to spawn trees, swamp/bog spawns more bushes. reduced spawn rate of binoculars and power bars.

--- Scripts/randomMapGenerator.js
+++ Scripts/randomMapGenerator.js
@@ -1,15 +1,19 @@
-//Johnny Neckar 11/22/2018
-//work in progress, mostly functional but needs to be linked and stuff. 
-//feel free to play around with it if you want.
-
-
+/*Johnny Neckar 11/22/2018
+function outputs tiles for a random map. takes desired map size as argument,
+function bounded between 20 and 256 mapsize. outputs array of strings, each 
+element being a tile specification in string format. function places diamonds 
+in random valid location.
+*/
 function generateRandomMap(size) {
+	
 	
 	if (size < 20)
 		size = 20;
 	if (size > 256)
 		size = 256;
 	
+	//create two empty 2d arrays of mapsize x mapsize. first represents terrain type,
+	//second represents object in tile.
 	let rMapTerrain = Array(size).fill().map(() => Array(size).fill(0));
 	let rMapObject = Array(size).fill().map(() => Array(size).fill("None"))
 	
@@ -39,6 +43,7 @@ function generateRandomMap(size) {
 		return;
 	}
 
+	//draw a group of forest tiles
 	function addForest(x, y) {
 		setSurrounding(1, x, y);
 		tile = [x,y];
@@ -49,6 +54,7 @@ function generateRandomMap(size) {
 		}
 	}
 
+	//draw a group of bog tiles
 	function addBog(x, y) {
 		setSurrounding(4, x, y);
 		tile = [x,y];
@@ -59,6 +65,7 @@ function generateRandomMap(size) {
 		}
 	}
 
+	//draw a group of swamp tiles	
 	function addSwamp(x, y) {
 		setSurrounding(5, x, y);
 		tile = [x,y];
@@ -70,6 +77,7 @@ function generateRandomMap(size) {
 		
 	}
 
+	//draw a group of water tiles
 	function addLake(x, y) {
 		setSurrounding(2, x, y);
 		tile = [x,y];
@@ -81,6 +89,7 @@ function generateRandomMap(size) {
 		
 	}
 
+	//draw roughly linear water tiles
 	function addRiver(x, y) {
 		
 		rMapTerrain[x][y] = 2;
@@ -120,87 +129,151 @@ function generateRandomMap(size) {
 		}
 	}
 
-	
+	//set number of tile groups randomly based on map size. number in max/min declarations can
+	//be adjusted to change density of tile groups (increasing number will reduce density)
 	var max = Math.ceil((size * size) / 300);
-	var min = Math.floor((size * size) / 750);
-	
+	var min = Math.floor((size * size) / 750);	
 	var numForest = Math.floor(Math.random() * (max - min)) + min;
 	var numBog = Math.floor(Math.random() * (max - min)) + min;	
 	var numSwamp = Math.floor(Math.random() * (max - min)) + min;
 	var numRiver = Math.floor(Math.random() * (max - min)) + min;
 	var numLake = Math.floor((Math.random() * (max - min) + min)/2);
 	
+	//call the draw functions randomly selected number of times
 	var i = 0;
 	while (i <= numForest) {
 		addForest(Math.floor(Math.random() * size), Math.floor(Math.random() * size));
 		i++;
 	}
-	
 	i = 0;
 	while (i <= numBog) {
 		addBog(Math.floor(Math.random() * size), Math.floor(Math.random() * size));
 		i++;		
 	}	
-	
 	i = 0;
 	while (i <= numSwamp) {
 		addSwamp(Math.floor(Math.random() * size), Math.floor(Math.random() * size));
 		i++;		
 	}	
-	
 	i = 0;
 	while (i <= numLake) {
 		addLake(Math.floor(Math.random() * size), Math.floor(Math.random() * size));
 		i++;		
 	}	
-
 	i = 0;
 	while (i <= numRiver) {
 		addRiver(Math.floor(Math.random() * size), Math.floor(Math.random() * size));
 		i++;		
 	}	
 
+	//this huge block add objects to tiles (items, obstacles, chests, powerbars)
+	//object probabilty can be adjusted based on terrain type
 	for (j=0; j < size; j++) {
 		for (k=0; k < size; k++) {
-			if (rMapTerrain[j][k] != 2) {
-				var roll = Math.random();
-				switch (true) {
-					case (roll < .8):
-						break;
-					case (roll < .833):
-						rMapObject[j][k] = "bush";
-						break;
-					case (roll < .866):
-						rMapObject[j][k] = "tree";
-						break;
-					case (roll < .899):
-						rMapObject[j][k] = "boulder";
-						break;
-					case (roll < .93):
-						rMapObject[j][k] = "power-bar";
-						break;	
-					case (roll < .95):
-						rMapObject[j][k] = "type1-treasure";
-						break;							
-					case (roll < .96):
-						rMapObject[j][k] = "type2-treasure";
-						break;		
-					case (roll < .97):
-						rMapObject[j][k] = "binoculars";
-						break;
-					case (roll < .98):
-						rMapObject[j][k] = "shears";
-						break;	
-					case (roll < .99):
-						rMapObject[j][k] = "jackhammer";
-						break;							
-					default:
-						rMapObject[j][k] = "chainsaw";
-				}
+			switch(true) {
+				case (rMapTerrain[j][k] == 2):
+					break;
+				case (rMapTerrain[j][k] == 0):
+					var roll = Math.random();
+					switch (true) {
+						case (roll < .8):
+							break;
+						case (roll < .82):
+							rMapObject[j][k] = "bush";
+							break;
+						case (roll < .85):
+							rMapObject[j][k] = "tree";
+							break;
+						case (roll < .905):
+							rMapObject[j][k] = "boulder";
+							break;
+						case (roll < .93):
+							rMapObject[j][k] = "power-bar";
+							break;	
+						case (roll < .95):
+							rMapObject[j][k] = "type1-treasure";
+							break;							
+						case (roll < .96):
+							rMapObject[j][k] = "type2-treasure";
+							break;		
+						case (roll < .97):
+							rMapObject[j][k] = "binoculars";
+							break;
+						case (roll < .98):
+							rMapObject[j][k] = "shears";
+							break;	
+						case (roll < .99):
+							rMapObject[j][k] = "jackhammer";
+							break;							
+						default:
+							rMapObject[j][k] = "chainsaw";
+					}
+					break;
+				case (rMapTerrain[j][k] == 4 || rMapTerrain[j][k] == 5):
+					var roll = Math.random();
+					switch (true) {
+						case (roll < .75):
+							break;
+						case (roll < .905):
+							rMapObject[j][k] = "bush";
+							break;
+						case (roll < .93):
+							rMapObject[j][k] = "power-bar";
+							break;	
+						case (roll < .95):
+							rMapObject[j][k] = "type1-treasure";
+							break;							
+						case (roll < .96):
+							rMapObject[j][k] = "type2-treasure";
+							break;		
+						case (roll < .97):
+							rMapObject[j][k] = "binoculars";
+							break;
+						case (roll < .98):
+							rMapObject[j][k] = "shears";
+							break;	
+						case (roll < .99):
+							rMapObject[j][k] = "jackhammer";
+							break;							
+						default:
+							rMapObject[j][k] = "chainsaw";
+					}
+					break;				
+				case (rMapTerrain[j][k] == 1):
+					var roll = Math.random();
+					switch (true) {
+						case (roll < .5):
+							break;
+						case (roll < .905):
+							rMapObject[j][k] = "tree";
+							break;
+						case (roll < .93):
+							rMapObject[j][k] = "power-bar";
+							break;	
+						case (roll < .95):
+							rMapObject[j][k] = "type1-treasure";
+							break;							
+						case (roll < .96):
+							rMapObject[j][k] = "type2-treasure";
+							break;		
+						case (roll < .97):
+							rMapObject[j][k] = "binoculars";
+							break;
+						case (roll < .98):
+							rMapObject[j][k] = "shears";
+							break;	
+						case (roll < .99):
+							rMapObject[j][k] = "jackhammer";
+							break;							
+						default:
+							rMapObject[j][k] = "chainsaw";
+					}
+					break;				
 			}
 		}
 	}
-						
+	
+	//find an empty tile and place diamonds
 	var tempX, tempY;
 	do {
 		tempX = Math.floor(Math.random() * size);
@@ -208,6 +281,7 @@ function generateRandomMap(size) {
 	} while (rMapTerrain[tempX][tempY] == 2 || rMapObject[tempX][tempY] != "None");
 	rMapObject[tempX][tempY] = "Diamonds";
 	
+	//save generated tiles to array of strings. sparsely populated (meadow tiles with no object don't get at entry)
 	tilesToSave = []
 	var tempTile;
 	for (j=0; j<size; j++) {
